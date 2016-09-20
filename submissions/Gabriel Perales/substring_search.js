6944484980230dc0b81b176981b0ad4b557cb207
Gabriel Perales <gabrielperales@gmail.com>
2016-09-20 21:07:55 +0100

    fixed edge case and code compiled with Google Closure Compiler

--- submissions/Gabriel Perales/substring_search.js
+++ submissions/Gabriel Perales/substring_search.js
@@ -1,7 +1,9 @@
-exports.substring_search = (words, sentence) =>
-  words.reduce((acc, curr) => {
-    const regexp = curr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    acc[curr] = (sentence.match(new RegExp(regexp, 'g')) || []).length;
-    return acc;
+exports.substring_search =  function(d, e) {
+  return d.reduce(function(b, c) {
+    var a = c.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"), a = new RegExp(a, "g");
+    for (b[c] = 0;a.lastIndex = (a.exec(e) || {index:-1}).index + 1;) {
+      b[c]++;
+    }
+    return b;
   }, {});
-
+};
